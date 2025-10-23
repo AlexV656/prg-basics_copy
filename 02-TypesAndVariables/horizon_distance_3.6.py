7800a35997c3252c24d5b9161b329e0c774e84c6
Oleksandr Vasyliev <vasylievalexandr2@gmail.com>
2025-10-23 19:49:34 +0200

    Lesson1.2

---
+++ 02-TypesAndVariables/horizon_distance_3.6.py
@@ -0,0 +1,8 @@
+import math 
+height= 0
+my_height = 1.72
+hotel_height = 20
+distance_to_horizon_km =3.57*math.sqrt(height)
+#print(distance_to_horizon_km)
+print(3.57*math.sqrt(my_height),'distance to horizon standing on the beach')
+print(3.57*math.sqrt(hotel_height+my_height),'distance to horizon standing on the balcon of the hotel')
