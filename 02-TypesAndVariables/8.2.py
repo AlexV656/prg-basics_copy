39a7bb9e9d29d509fd7f9771afb2452287c30a4f
Oleksandr Vasyliev <vasylievalexandr2@gmail.com>
2025-10-28 23:19:52 +0100

    8_part_all_tasks_complited

---
+++ 02-TypesAndVariables/8.2.py
@@ -0,0 +1,4 @@
+import math
+r = float(input('Enter radius of a circle: '))
+print (f'circumference = {2*r*math.pi:.4f}, area = {math.pi*r**2:.4f}')
+
---
+++ 02-TypesAndVariables/8.3.py
@@ -0,0 +1,12 @@
+###
+# A program that reads temperature in degrees Celsius from the keyboard.
+# Use comments to briefly describe the program's algorithm.
+#
+
+# ...
+# ...
+# ...
+temperature_in_C = float(input('enter temperature in degrees Celsius: '))
+print(f'temperature in Kelvin = {temperature_in_C+273.15}K')#Formula = x°C + 273.15 = (x+273,15)K
+print(f'temperature in Fahrenheit = {(temperature_in_C*9/5)+32}F')#Formula = (0°C × 9/5) + 32 = 32°F
+
